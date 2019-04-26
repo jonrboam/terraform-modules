10abf278707bf1f07b705a63fa598cafe3e02c2d
Jon Boam <jonrboam@gmail.com>
2019-04-26 13:29:50 +0100

    correct

--- users/main.tf
+++ users/main.tf
@@ -54,3 +54,91 @@ data "aws_iam_policy_document" "user_profile_self_service" {
   }
 }
 
+resource "aws_iam_user_policy" "user_profile_self_service" {
+  name    = "UserProfileSelfService"
+  count   = "${length(var.name)}"
+  policy  = "${data.aws_iam_policy_document.user_profile_self_service.json}"
+  user    = "${aws_iam_user.user.*.name}"
+}
+
+data "aws_iam_policy_document" "enforce_mfa" {
+  statement {
+    condition {
+      test      = "Null"
+      values    = [
+        "true"
+      ]
+      variable  = "aws:MultiFactorAuthPresent"
+    }
+    effect = "Deny"
+    not_actions = [
+      "iam:*LoginProfile",
+      "iam:*MFADevice",
+      "iam:ChangePassword",
+      "iam:GetAccountPasswordPolicy",
+      "iam:GetAccountSummary",
+      "iam:List*MFADevices",
+      "iam:ListAccountAliases",
+      "iam:ListUsers"
+    ]
+    resources   = [
+      "*"
+    ]
+    sid         = "DenyEverythingExceptForBelowUnlessMFAd"
+  }
+
+  statement {
+    condition {
+      test        = "Null"
+      values      = [
+        "true"
+      ]
+      variable    = "aws:MultiFactorAuthPresent"
+    }
+    effect        = "Deny"
+    actions       = [
+      "iam:*LoginProfile",
+      "iam:*MFADevice",
+      "iam:ChangePassword",
+      "iam:GetAccountSummary"
+    ]
+    not_resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/${aws_iam_user.user.*.name}",
+      "${aws_iam_user.user.*.arn}"
+    ]
+    sid           = "DenyIamAccessToOtherAccountsUnlessMFAd"
+  }
+}
+
+resource "aws_iam_user_policy" "enforce_mfa" {
+  name    = "EnforceMFA"
+  count   = "${length(var.name)}"
+  policy  = "${data.aws_iam_policy_document.enforce_mfa.json}"
+  user    = "${aws_iam_user.user.*.name}"
+}
+
+resource "aws_iam_user_policy_attachment" "manage_specific_credentials" {
+  policy_arn = "arn:aws:iam::aws:policy/IAMSelfManageServiceSpecificCredentials"
+  count      = "${length(var.name)}"
+  user       = "${aws_iam_user.user.*.name}"
+}
+
+resource "aws_iam_user_policy_attachment" "change_password" {
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+  count      = "${length(var.name)}"
+  user       = "${aws_iam_user.user.*.name}"
+}
+
+resource "aws_iam_user_policy_attachment" "manage_ssh_keys" {
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserSSHKeys"
+  count      = "${length(var.name)}"
+  user       = "${aws_iam_user.user.*.name}"
+}
+
+resource "aws_iam_user_policy_attachment" "iam_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+  count      = "${length(var.name)}"
+  user       = "${aws_iam_user.user.*.name}"
+}
+
+
