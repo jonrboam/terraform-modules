7526187d6db36fbddfb33c723893fb8ac27570e2
Jon Boam <jonrboam@gmail.com>
2019-04-26 11:40:45 +0100

    Updated to handle count in output

--- users/outputs.tf
+++ users/outputs.tf
@@ -1,14 +1,14 @@
 output "arn" {
   description = "The ARN assigned by AWS for this user."
-  value       = "${aws_iam_user.user.arn}"
+  value       = "${aws_iam_user.user.*.arn}"
 }
 
 output "name" {
   description = "The user's name."
-  value       = "${aws_iam_user.user.name}"
+  value       = "${aws_iam_user.user.*.name}"
 }
 
 output "unique_id" {
   description = "The unique ID assigned by AWS. "
-  value       = "${aws_iam_user.user.unique_id}"
+  value       = "${aws_iam_user.user.*.unique_id}"
 }
